aca7e7218c16b08b953d112f66373315f3895501
iyansr <ikhyan7@gmail.com>
2023-08-13 19:53:33 +0800

    feat(jobs): List Jobs API

--- src/api/jobs/findJobs.ts
+++ src/api/jobs/findJobs.ts
@@ -3,12 +3,31 @@ import type { PostgrestSingleResponse } from '@supabase/supabase-js';
 import supabase from '@/lib/supabase';
 import type { Job } from '@/types';
 
-const findJobs = async (): Promise<PostgrestSingleResponse<Job[]>> => {
-  const res = await supabase.from('jobs').select(`
+type Params = {
+  name?: string;
+  company?: string;
+  category?: string;
+  /**@default 0 */
+  from?: number;
+  /**@default 10 */
+  to?: number;
+};
+
+const findJobs = async (params: Params): Promise<PostgrestSingleResponse<Job[]>> => {
+  const res = await supabase
+    .from('jobs')
+    .select(
+      `
    *,
    company:company_id (*),
    category:category_id (*)
-  `);
+  `,
+    )
+    .like('name', `%${params.name}%`)
+    .like('company.name', `%${params.name}%`)
+    .filter('category.name', 'eq', params.category)
+    .range(params.from ?? 0, params.to ?? 10);
+
   return res;
 };
 
